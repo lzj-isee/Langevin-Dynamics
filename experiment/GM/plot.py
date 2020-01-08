e84f42ca9fecd5d50cdcb621896bb68112153364
lzj-isee <51073165+lzj-isee@users.noreply.github.com>
2020-01-08 16:56:20 +0800

    2020/1/8

--- experiment/GM/plot.py
+++ experiment/GM/plot.py
@@ -1,18 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-lens=500000
+lens=1000000
+Ys=np.load('./ref.npy')
+Xs=np.linspace(-6,6,len(Ys))
 
 
 plt.figure()
 sgld=np.load('./result_samples/SGLD/samples.npy')
 sgld=sgld[len(sgld)-lens:len(sgld)]
-#plt.hist2d(sgld[:,0],sgld[:,1],bins=40,range=[[-6,6],[-6,6]])
-plt.hist(sgld[:,0],bins=40,range=[-6,6])
+plt.hist2d(sgld[:,0],sgld[:,1],bins=40,range=[[-6,6],[-6,6]])
 
 plt.figure()
 sgulmcmc=np.load('./result_samples/SG_UL_MCMC/samples.npy')
 sgulmcmc=sgulmcmc[len(sgulmcmc)-lens:len(sgulmcmc)]
-#plt.hist2d(sgulmcmc[:,0],sgulmcmc[:,1],bins=40,range=[[-6,6],[-6,6]])
-plt.hist(sgulmcmc[:,0],bins=40,range=[-6,6])
+plt.hist2d(sgulmcmc[:,0],sgulmcmc[:,1],bins=40,range=[[-6,6],[-6,6]])
+
+
+plt.figure()
+plt.plot(Xs,Ys[:,0])
+plt.figure()
+plt.plot(Xs,Ys[:,1])
 
 plt.show()
---
+++ experiment/GM/ref_calc.py
@@ -0,0 +1,25 @@
+import numpy as np
+from tqdm import tqdm
+def pdf(x,sample):
+    result0=(2*np.exp(-(x-sample[0])**2/2)+np.exp(-(x+sample[0])**2/2))/(3*np.sqrt(2*np.pi))
+    result1=(2*np.exp(-(x-sample[1])**2/2)+np.exp(-(x+sample[1])**2/2))/(3*np.sqrt(2*np.pi))
+    return np.array([result0,result1])
+samples=np.load('./dataset/a.npy')
+lens=len(samples)
+
+Xs=np.linspace(-6,6,120)
+Ys=[]
+
+for x in tqdm(Xs):
+    temp=np.zeros(2)
+    for sample in samples:
+        temp+=np.log(pdf(x,sample))
+    temp=np.exp(temp/lens)
+    Ys.append(temp)
+Ys=np.array(Ys)
+np.save('./ref.npy',Ys)
+
+    
+
+
+
